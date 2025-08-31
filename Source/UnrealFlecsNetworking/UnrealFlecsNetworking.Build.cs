0f012e6c73898bf2faded29ba9b267b96070f0f4
Elie Wiese-Namir <65688235+Reddy-dev@users.noreply.github.com>
2025-08-31 19:20:37 -0400

    Unreal Initial Commit

---
+++ Source/UnrealFlecsNetworking/UnrealFlecsNetworking.Build.cs
@@ -0,0 +1,41 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealFlecsNetworking : ModuleRules
+{
+	public UnrealFlecsNetworking(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				ModuleDirectory,
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+			}
+			);
+	}
+}
